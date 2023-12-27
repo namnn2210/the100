f32d4dba1daba5f8a18030aef33c3d2afed50803
namng <nam.nn@bidgear.com>
2023-12-28 03:52:36 +0700

    edit

--- the100_platform/homepage/views.py
+++ the100_platform/homepage/views.py
@@ -1,9 +1,21 @@
 from django.shortcuts import render
-
+from products.models import Product
+from product_images.models import ProductImage
+from django.forms.models import model_to_dict
 
 # Create your views here.
 def index(request):
-    return render(request=request, template_name='index-shop.html')
+    list_products = Product.objects.all().order_by('-view')[:8]
+    list_product_dict = []
+    for item in list_products:
+        list_product_dict.append(model_to_dict(item))
+    for item in list_product_dict:
+        list_img_dict = []
+        list_imgs = ProductImage.objects.filter(product_id=item['id'])
+        for img in list_imgs:
+            list_img_dict.append(model_to_dict(img))
+        item['images'] = list_img_dict
+    return render(request=request, template_name='index-shop.html',context={'list_products':list_product_dict})
 
 
 def about(request):
