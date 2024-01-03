3ef9e7e081774c48f06c4f0154248ba48af28b7f
Ngo Ngoc Nam <namngocngo22@gmail.com>
2024-01-03 09:27:38 +0700

    edit

--- the100_platform/static/admin/assets/js/add_product.js
+++ the100_platform/static/admin/assets/js/add_product.js
@@ -1,25 +1,26 @@
 var lastLevelCategory = 0
 
 function getCookie(name) {
-        let cookieValue = null;
-        if (document.cookie && document.cookie !== '') {
-            const cookies = document.cookie.split(';');
-            for (let i = 0; i < cookies.length; i++) {
-                const cookie = cookies[i].trim();
-                if (cookie.substring(0, name.length + 1) === (name + '=')) {
-                    cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-                    break;
-                }
+    let cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        const cookies = document.cookie.split(';');
+        for (let i = 0; i < cookies.length; i++) {
+            const cookie = cookies[i].trim();
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
             }
         }
-        return cookieValue;
-    };
+    }
+    return cookieValue;
+};
+
 function populateList(categories, childrenClass) {
     // Find the div in the DOM
     const div = document.querySelector('.' + childrenClass);
     var number = childrenClass.split('-')[1]
     var new_number = parseInt(number) + 1
-    var newChildren = 'children-'+new_number
+    var newChildren = 'children-' + new_number
     // Create a new 'ul' element
     const ul = document.createElement('ul');
     ul.className = 'list-unstyled scrollable-list'; // Add any classes you need for styling
@@ -64,48 +65,76 @@ function chooseCategory(categoryId, children, displayName) {
         var chosenCategories = document.getElementById('chosenCategories')
         chosenCategories.innerHTML = ''
         chosenCategories.innerHTML = displayName
-    }
-    else {
+    } else {
         var chosenCategories = document.getElementById('chosenCategories').textContent
         const newChosenCategories = chosenCategories + ' > ' + displayName
         chosenCategories.innerHTML = newChosenCategories
     }
     var dataToSend = {
-        categoryId:categoryId
+        categoryId: categoryId
     }
     fetch("/admin/category/children/", {
-            method: 'POST',
-            headers: {
-                "X-CSRFToken": csrftoken // Include CSRF token in headers
-            },
-            body: JSON.stringify(dataToSend),
-        })
+        method: 'POST',
+        headers: {
+            "X-CSRFToken": csrftoken // Include CSRF token in headers
+        },
+        body: JSON.stringify(dataToSend),
+    })
         .then(response => response.json())
         .then(data => {
             // 'data' is now the JSON response from Django
             const categories = JSON.parse(data.categories);
-            populateList(categories,children);
+            populateList(categories, children);
         })
 }
 
 function saveChoosenCategory() {
-    console.log('Submit category: ', lastLevelCategory)
     const csrftoken = getCookie('csrftoken');
     var dataToSend = {
-        categoryId:lastLevelCategory
+        categoryId: lastLevelCategory
     }
     fetch("/shopee/get_attributes/", {
-            method: 'POST',
-            headers: {
-                "X-CSRFToken": csrftoken // Include CSRF token in headers
-            },
-            body: JSON.stringify(dataToSend),
-        })
+        method: 'POST',
+        headers: {
+            "X-CSRFToken": csrftoken // Include CSRF token in headers
+        },
+        body: JSON.stringify(dataToSend),
+    })
         .then(response => response.json())
         .then(data => {
             // 'data' is now the JSON response from Django
-            console.log(data.data)
+            console.clear();
+            loadFormAfterSaveCate(data.data.response.attribute_list);
         })
+}
+
+function loadFormAfterSaveCate(attributeLists) {
+    console.log(attributeLists)
+    quitModal()
+    document.querySelector('#detail-info').removeAttribute('style');
+    const divDetail = document.createElement('div');
+    attributeLists.forEach(attribute => {
+        const div = document.createElement('div');
+        div.setAttribute('class', 'form-group');
+
+        const label = document.createElement('label');
+        label.setAttribute('for', attribute.attribute_id);
+        label.text = attribute.display_attribute_name;
+        div.appendChild(label);
+        const select = document.createElement('select');
+        select.setAttribute('id', attribute.attribute_id);
+        select.setAttribute('class', 'form-control');
+
+        attribute.attribute_value_list.forEach(valueList => {
+            const option = document.createElement('option');
+            option.value = valueList.value_id;
+            option.text = valueList.display_value_name;
+            select.appendChild(option)
+        });
+        div.appendChild(select);
+        divDetail.appendChild(div);
+    });
+    document.querySelector('#detail-info-content').appendChild(divDetail);
 }
 
 function quitModal() {
