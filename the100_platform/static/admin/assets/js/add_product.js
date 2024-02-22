02bbafde27218de15cea7146c2aa939122455f57
LongLTDev <longlaitiendev@gmail.com>
2024-02-22 22:39:46 +0700

    Revert "ADD PRODUCT: fix"
    This reverts commit a36c507a5c07563eb3f95b0feb87a5224b5bfe2b.

--- the100_platform/static/admin/assets/js/add_product.js
+++ the100_platform/static/admin/assets/js/add_product.js
@@ -2,6 +2,363 @@ var lastLevelCategory = 0;
 var editItems = {
 }
 
+function getCookie(name) {
+  let cookieValue = null;
+  if (document.cookie && document.cookie !== "") {
+    const cookies = document.cookie.split(";");
+    for (let i = 0; i < cookies.length; i++) {
+      const cookie = cookies[i].trim();
+      if (cookie.substring(0, name.length + 1) === name + "=") {
+        cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+        break;
+      }
+    }
+  }
+  return cookieValue;
+}
+
+function populateList(categories, childrenClass) {
+  // Find the div in the DOM
+  const div = document.querySelector("." + childrenClass);
+  const number = childrenClass.split("-")[1];
+  const new_number = parseInt(number) + 1;
+  const newChildren = "children-" + new_number;
+  // Create a new 'ul' element
+  const ul = document.createElement("ul");
+  ul.className = "list-unstyled scrollable-list"; // Add any classes you need for styling
+
+  // Clear existing content in the div
+  div.innerHTML = "";
+
+  // Loop through each category and add it to the list
+  categories.forEach((category) => {
+    console.log(category);
+    // Create a new 'li' element
+    const li = document.createElement("li");
+
+    // Add category name
+    const spanName = document.createElement("span");
+    spanName.textContent = category.fields.display_category_name;
+    spanName.setAttribute("id", `${category.pk}`);
+    spanName.setAttribute('data-can-click', `${category.fields.has_children}`);
+    spanName.setAttribute(
+      "onclick",
+      `chooseCategory(this, ${category.pk}, '${newChildren}', '${category.fields.display_category_name}')`
+    );
+    li.appendChild(spanName);
+
+    // Check if the category has children and add an indicator
+    if (category.fields.has_children) {
+      const spanArrow = document.createElement("span");
+      spanArrow.innerHTML = ' &rsaquo; ';
+      spanArrow.classList.add("category-item-right");
+      spanArrow.style = 'font-size: 20px';
+      li.appendChild(spanArrow);
+    }
+
+    // Append the 'li' element to the 'ul'
+    ul.appendChild(li);
+  });
+
+  // Append the 'ul' to the div
+  div.appendChild(ul);
+}
+
+function chooseCategory(ctx, categoryId, children, displayName) {
+  // Highlight to item is clicked
+  let parentEle = ctx.parentElement.parentElement;
+  let listChildrensEle = parentEle.children;
+  for (let i = 0; i < listChildrensEle.length; i++) {
+    if (listChildrensEle[i].hasAttribute('data-clicked')){
+      listChildrensEle[i].removeAttribute('data-clicked');
+    }
+  }
+  ctx.parentElement.setAttribute('data-clicked', true);
+
+
+  // Check if item is not exist then save item to object
+  var currentIndex = children.split('-')[1];
+  if (!editItems['lv' + (currentIndex - 1)]){
+    editItems['lv' + (currentIndex - 1)] = {};
+    editItems['lv' + (currentIndex - 1)].id = categoryId;
+    editItems['lv' + (currentIndex - 1)].name = ctx.textContent.trim();
+  } else {
+    editItems['lv' + (currentIndex - 1)].id = categoryId;
+    editItems['lv' + (currentIndex - 1)].name = ctx.textContent.trim();
+  }
+
+  // Reset list all items behind current list items (currentIndex)
+  // range = 6: user setup, not logic of system
+  for (var i = currentIndex; i < 6; i++) {
+    if (document.querySelector('.children-' + i)){
+      document.querySelector('.children-' + i).innerHTML = '';
+      delete editItems['lv' + (i)];
+    }
+  }
+
+  // ON/OFF save change button
+  var isSaveChange = ctx.getAttribute('data-can-click').toLowerCase().toString();
+  if (isSaveChange == 'true'){
+    document.getElementById('btn-save-change').setAttribute('disabled', true);
+  } else {
+    document.getElementById('btn-save-change').removeAttribute('disabled');
+  }
+
+  // Show list items is choosen to views
+  var listItemSelectedStr = '';
+  var listKeys = Object.keys(editItems);
+  for (let i = 0; i < listKeys.length; i++) {
+    listItemSelectedStr += editItems[listKeys[i]].name + (i < listKeys.length - 1 ? ' &rsaquo; ' : '');
+  }
+  document.getElementById("chosenCategories").innerHTML = "Đã chọn: " + '<span style="font-weight: 600">' + listItemSelectedStr + '</span>';
+
+  let chosenCategories;
+  lastLevelCategory = categoryId;
+  // console.log("Last level category: ", lastLevelCategory);
+  const csrftoken = getCookie("csrftoken");
+  // if (children === "children-1") {
+  //   chosenCategories = document.getElementById("chosenCategories");
+  //   chosenCategories.innerHTML = "";
+  //   chosenCategories.innerHTML = displayName;
+  // } else {
+  //   chosenCategories = document.getElementById("chosenCategories").textContent;
+  //   chosenCategories.innerHTML = chosenCategories + " > " + displayName;
+  // }
+  const dataToSend = {
+    categoryId: categoryId,
+  };
+  fetch("/admin/category/children/", {
+    method: "POST",
+    headers: {
+      "X-CSRFToken": csrftoken, // Include CSRF token in headers
+    },
+    body: JSON.stringify(dataToSend),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      // 'data' is now the JSON response from Django
+      const categories = JSON.parse(data.categories);
+      populateList(categories, children);
+    });
+}
+
+
+function saveChoosenCategory() {
+  const csrftoken = getCookie("csrftoken");
+  const dataToSend = {
+    categoryId: lastLevelCategory,
+  };
+  console.clear();
+
+
+  // Show list items is choosen into input
+  var listItemSelectedStr = '';
+  var listKeys = Object.keys(editItems);
+  for (let i = 0; i < listKeys.length; i++) {
+    listItemSelectedStr += editItems[listKeys[i]].name + (i < listKeys.length - 1 ? ' > ' : '');
+  }
+  document.querySelector("input[id=category]").value =listItemSelectedStr;
+
+  fetch("/shopee/get_attributes/", {
+    method: "POST",
+    headers: {
+      "X-CSRFToken": csrftoken, // Include CSRF token in headers
+    },
+    body: JSON.stringify(dataToSend),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      // 'data' is now the JSON response from Django
+      console.log("Getting attributes...");
+      console.log(data.data);
+      loadFormAfterSaveCate(data.data.response.attribute_list);
+    });
+
+  // fetch("/shopee/get_brands/", {
+  //     method: 'POST',
+  //     headers: {
+  //         "X-CSRFToken": csrftoken // Include CSRF token in headers
+  //     },
+  //     body: JSON.stringify(dataToSend),
+  // })
+  //     .then(response => response.json())
+  //     .then(data => {
+  //         // 'data' is now the JSON response from Django
+  //         console.log('Getting brands...')
+  //         console.log(data.data.response)
+  //         loadBrands(data.data.response);
+  //     })
+  //
+  //
+  // fetch("/shopee/get_dts_limit/", {
+  //     method: 'POST',
+  //     headers: {
+  //         "X-CSRFToken": csrftoken // Include CSRF token in headers
+  //     },
+  //     body: JSON.stringify(dataToSend),
+  // })
+  //     .then(response => response.json())
+  //     .then(data => {
+  //         // 'data' is now the JSON response from Django
+  //         console.log('Getting day to ship...')
+  //         console.log(data.data.response.days_to_ship_limit)
+  //         var dayToShipInput = $('#days-to-ship-input');
+  //         if (data.data.response.days_to_ship_limit) {
+  //             dayToShipInput.attr({
+  //                 "max": data.data.response.days_to_ship_limit.max_limit,
+  //                 "min": data.data.response.days_to_ship_limit.min_limit
+  //             });
+  //         }
+  //     })
+  //
+  //
+  fetch("/shopee/get_size_chart/", {
+    method: "POST",
+    headers: {
+      "X-CSRFToken": csrftoken, // Include CSRF token in headers
+    },
+    body: JSON.stringify(dataToSend),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      // 'data' is now the JSON response from Django
+      console.log("Getting size chart...");
+      console.log(data.data);
+    });
+  //
+  // fetch("/shopee/get_item_limit/", {
+  //     method: 'POST',
+  //     headers: {
+  //         "X-CSRFToken": csrftoken // Include CSRF token in headers
+  //     },
+  //     body: JSON.stringify(dataToSend),
+  // })
+  //     .then(response => response.json())
+  //     .then(data => {
+  //         // 'data' is now the JSON response from Django
+  //         console.log('Getting item limit...')
+  //         console.log(data.data)
+  //     })
+  //
+  // fetch("/shopee/get_channel_list/", {
+  //     method: 'POST',
+  //     headers: {
+  //         "X-CSRFToken": csrftoken // Include CSRF token in headers
+  //     },
+  //     body: JSON.stringify(dataToSend),
+  // })
+  //     .then(response => response.json())
+  //     .then(data => {
+  //         // 'data' is now the JSON response from Django
+  //         console.log('Getting channel list...')
+  //         console.log(data.data)
+  //     })
+
+  quitModal();
+}
+
+function loadFormAfterSaveCate(attributeLists) {
+  var detailInfo = document.querySelector("#detail-info-content");
+  detailInfo.innerHTML = "";
+  document.querySelector("#detail-info").removeAttribute("style");
+  const divDetail = document.createElement("div");
+  divDetail.setAttribute("class", "row");
+  attributeLists.forEach((attribute) => {
+    const div = document.createElement("div");
+    div.setAttribute("class", "form-group col-md-6 my-2");
+
+    const label = document.createElement("label");
+    label.setAttribute("for", attribute.attribute_id);
+    label.text = attribute.display_attribute_name;
+    label.innerText = attribute.display_attribute_name;
+    div.appendChild(label);
+
+    if (attribute.input_type == "COMBO_BOX") {
+      const select = document.createElement("select");
+      select.setAttribute("id", attribute.attribute_id);
+      select.setAttribute("class", "form-control col-md-6");
+      attribute.attribute_value_list.forEach((valueList) => {
+        const option = document.createElement("option");
+        option.value = valueList.value_id;
+        option.text = valueList.display_value_name;
+        select.appendChild(option);
+      });
+      div.appendChild(select);
+    } else {
+      const input = document.createElement("input");
+      let type = "";
+      input.setAttribute("id", attribute.attribute_id);
+      input.setAttribute("class", "form-control");
+      if (attribute.input_validation_type == "DATE_TYPE") {
+        type = "date";
+      } else {
+        type = "text";
+      }
+      input.setAttribute("type", type);
+      div.appendChild(input);
+    }
+    divDetail.appendChild(div);
+  });
+  detailInfo.appendChild(divDetail);
+}
+
+function loadBrands(attribute) {
+  var detailInfo = document.querySelector("#detail-info-content");
+  const div = document.createElement("div");
+  div.setAttribute("class", "form-group col-md-6 my-2");
+
+  const label = document.createElement("label");
+  label.setAttribute("for", attribute.attribute_id);
+  label.text = "Thương hiệu";
+  label.innerText = "Thương hiệu";
+  div.appendChild(label);
+
+  // if (attribute.input_type == 'DROP_DOWN') {
+  const select = document.createElement("select");
+  select.setAttribute("id", attribute.attribute_id);
+  select.setAttribute("class", "form-control col-md-6");
+  attribute.brand_list.forEach((valueList) => {
+    const option = document.createElement("option");
+    option.value = valueList.brand_id;
+    option.text = valueList.display_brand_name;
+    select.appendChild(option);
+  });
+  div.appendChild(select);
+  // } else {
+  //     const input = document.createElement('input');
+  //     let type = '';
+  //     input.setAttribute('id', attribute.attribute_id);
+  //     input.setAttribute('class', 'form-control');
+  //     if (attribute.input_validation_type == 'DATE_TYPE') {
+  //         type = 'date'
+  //     } else {
+  //         type = 'text'
+  //     }
+  //     input.setAttribute('type', type);
+  //     div.appendChild(input);
+  // }
+
+  detailInfo.appendChild(div);
+}
+
+function quitModal() {
+  const modalForm = document.getElementById("LoginForm");
+  const modal = bootstrap.Modal.getInstance(modalForm);
+  modal.hide();
+}
+
+function ready(cb) {
+  document.readyState === "loading"
+    ? document.addEventListener("DOMContentLoaded", function () {
+        cb();
+      })
+    : cb();
+}
+
+ready(function () {
+  initEvent();
+});
+
 function initData() {}
 
 function initEvent() {}
