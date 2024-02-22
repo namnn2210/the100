d06acab741acd544b7cb1a086c8d56c8e6cce64c
LongLTDev <longlaitiendev@gmail.com>
2024-02-23 00:30:02 +0700

    Add cate:  ini

--- the100_platform/categories/views.py
+++ the100_platform/categories/views.py
@@ -1,8 +1,39 @@
-from rest_framework.generics import ListAPIView
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Category
-from .serializers import CategorySerializer
+from .forms import CategoryForm
 
+def category_list(request):
+    categories = Category.objects.all()
+    return render(request, 'list.html', {'categories': categories})
 
-class CategoryListView(ListAPIView):
-    queryset = Category.objects.all()
-    serializer_class = CategorySerializer
+def category_detail(request, pk):
+    category = get_object_or_404(Category, pk=pk)
+    return render(request, 'detail.html', {'category': category})
+
+def category_create(request):
+    if request.method == 'POST':
+        form = CategoryForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('category_list')
+    else:
+        form = CategoryForm()
+    return render(request, 'form.html', {'form': form})
+
+def category_update(request, pk):
+    category = get_object_or_404(Category, pk=pk)
+    if request.method == 'POST':
+        form = CategoryForm(request.POST, instance=category)
+        if form.is_valid():
+            form.save()
+            return redirect('list')
+    else:
+        form = CategoryForm(instance=category)
+    return render(request, 'form.html', {'form': form})
+
+def category_delete(request, pk):
+    category = get_object_or_404(Category, pk=pk)
+    if request.method == 'POST':
+        category.delete()
+        return redirect('category_list')
+    return render(request, 'category_confirm_delete.html', {'category': category})
